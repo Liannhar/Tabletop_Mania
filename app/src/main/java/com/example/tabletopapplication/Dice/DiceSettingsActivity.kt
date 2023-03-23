c02926858e1cea82bc1b8e14429d2e7e293aabc0
Prosefionol <ilya_kuper@mail.ru>
2023-03-23 13:47:53 +0300

    add data transfer between DiceActivity

--- app/src/main/java/com/example/tabletopapplication/Dice/DiceSettingsActivity.kt
+++ app/src/main/java/com/example/tabletopapplication/Dice/DiceSettingsActivity.kt
@@ -3,6 +3,8 @@ package com.example.tabletopapplication.Dice
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.Spinner
@@ -11,6 +13,7 @@ import com.example.tabletopapplication.R
 class DiceSettingsActivity : AppCompatActivity() {
 
     val dices = arrayOf("d6", "d8", "d12", "d100")
+    var selectedDice: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,9 +23,25 @@ class DiceSettingsActivity : AppCompatActivity() {
         val arrayAdapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item,dices)
         arrayAdapter.setDropDownViewResource(R.layout.custom_dropdown)
         spinner.adapter = arrayAdapter
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                selectedDice = when (p2) {
+                    0 -> 6
+                    1 -> 8
+                    2 -> 12
+                    3 -> 100
+                    else -> 6
+                }
+            }
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
+            }
+        }
 
         findViewById<Button>(R.id.dice_setting_button).setOnClickListener {
-            val intent = Intent(this, DiceResultActivity::class.java)
+            val intent = Intent(this, DiceResultActivity::class.java).apply {
+                putExtra("dice", selectedDice)
+            }
             startActivity(intent)
         }
 
