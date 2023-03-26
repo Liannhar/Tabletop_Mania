53d3cc1f8cf83594d833cc83c91f45dd0f89cf09
Prosefionol <ilya_kuper@mail.ru>
2023-03-26 09:48:57 +0300

    ready to merge

--- app/src/main/java/com/example/tabletopapplication/presentationlayer/activities/DiceSettingsActivity.kt
+++ app/src/main/java/com/example/tabletopapplication/presentationlayer/activities/DiceSettingsActivity.kt
@@ -1,26 +1,25 @@
 package com.example.tabletopapplication.presentationlayer.activities
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.Spinner
+import android.widget.*
 import com.example.tabletopapplication.R
+import com.example.tabletopapplication.presentationlayer.activities.GameSelectionActivity
 
 class DiceSettingsActivity : AppCompatActivity() {
 
-    private val dices = arrayOf("d6", "d8", "d12", "d100")
-    var selectedDice: Int = 6
-
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings_dice)
 
+        var selectedDice = 6
+        val dices = arrayOf("d6", "d8", "d12", "d100")
         val spinner = findViewById<Spinner>(R.id.dice_setting_spinner)
-        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item,dices)
+        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, dices)
         arrayAdapter.setDropDownViewResource(R.layout.custom_dropdown)
         spinner.adapter = arrayAdapter
         spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
@@ -42,6 +41,12 @@ class DiceSettingsActivity : AppCompatActivity() {
             val intent = Intent(this, DiceResultActivity::class.java).apply {
                 putExtra(DiceConstants.SERIALIZABLE_DICE_NAME, selectedDice)
             }
+            startActivity(intent)
+        }
+
+        // Здесь задумывается переход на экран с игрой (сейчас реализован переход на главную страницу)
+        findViewById<ImageView>(R.id.dice_setting_goback).setOnClickListener {
+            val intent = Intent(this, GameSelectionActivity::class.java)
             startActivity(intent)
         }
     }
