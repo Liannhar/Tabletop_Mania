ab51a41cddaf778b37dc4e75cd1b3f6ee4377d31
Prosefionol <ilya_kuper@mail.ru>
2023-04-20 18:10:40 +0300

    preparation for work

---
+++ app/src/main/java/com/tabletop/tabletopapplication/presentationlayer/activities/HourglassActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tabletopapplication.presentationlayer.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HourglassActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hourglass)
+    }
+}
